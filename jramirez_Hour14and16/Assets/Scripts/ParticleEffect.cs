d3b90dcee78eef37f366a9da7f272d2d98ab42ec
ButterfingerBB <jargamer06@gmail.com>
2025-05-18 23:52:59 -0700

    Spiced up particle effects for future use

---
+++ jramirez_Hour14and16/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/* 
+Changes made to particles:
+1. Shape, radius and speed were modified (Cone shaped spread, 50% bigger radius, 2 times as fast).
+2. Particles actually slow down over time, with a speed multiplier of 0.8.
+3. Changed particle color to blue.
+4. Particles now have a lot more collision power, somewhat similar to a tornado.
+5. Angular velocity set to 135 degrees, allowing the sphere to float above the particles for a bit longer.
+*/
