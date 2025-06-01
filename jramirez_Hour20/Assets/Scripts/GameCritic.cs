bff68277c36aa0e636efbefb2cbbffc57d65f881
ButterfingerBB <jargamer06@gmail.com>
2025-05-31 21:26:35 -0700

    Created what feels like a mobile game..
    Oil slicks were harder to add than I thought...

---
+++ jramirez_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,22 @@
+/*
+
+1. Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+I feel like it's quite difficult to get a good score on this game mostly because of two things: 
+a point of view that doesn't cover the farthest edges of the map, and the fact that powerups barely do anything. To be
+fair, obstacles aren't much of a detriment either. 
+
+2. What 2 changes would you do to make it easier or harder?
+
+3. List 2 things that would give it a "wow" factor.
+
+4. What 2 parts of the game are fun?
+
+5. What 2 parts of the game are tedious?
+
+Changes made to obstacles and powerups:
+-
+-
+-
+-
+
+*/
